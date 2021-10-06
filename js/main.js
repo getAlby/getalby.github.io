669c46dbe916917eae874c0add2dd18b13f6e754
Albin CAUDERLIER <albin.cauderlier@gmail.com>
2021-10-06 13:11:07 +0200

    Design - Add to Browser btn on JS + logo + content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 
 
 $(document).ready(function () {
+
+	var add_alby_btn = document.getElementById("add_alby_btn");
+	add_alby_btn.innerHTML = '<a href="#" class="btn btn-outline-dark bg-white btn-lg rounded-pill px-3"><b><i data-feather="chrome"></i> Add to Chrome</b></a>';
+
 	//Display the icones
 	feather.replace();
 
