ebb93c7db5cd2ce81d858dddc0de86560774703c
Albin CAUDERLIER <albin.cauderlier@gmail.com>
2021-10-18 15:35:15 +0200

    JS - Detecting the Firefox or Google Chrome browser + button display

--- js/main.js
+++ js/main.js
@@ -11,20 +11,21 @@ $(document).ready(function () {
 	var add_alby_btn = document.getElementById("add_alby_btn");
 
 	// Firefox
-	// if( is_firefox() ) {
-	// 	add_alby_btn_header.innerHTML = '<a href="'+firefox_extension_url+'" class="btn btn-outline-dark bg-white rounded-pill px-4 my-4"><b>Add to Firefox</b></a>';
-	// 	add_alby_btn.innerHTML = '<a href="'+firefox_extension_url+'" class="btn btn-outline-dark bg-white btn-lg rounded-pill"><img src="images/browsers/firefox-icon.png" style="max-height: 32px;"/><b> Add to Firefox</b></a>';
-	// }
+	if( is_firefox() ) {
+		add_alby_btn_header.innerHTML = '<a href="'+firefox_extension_url+'" class="btn btn-outline-dark bg-white rounded-pill px-4 my-4"><b>Add to Firefox</b></a>';
+		add_alby_btn.innerHTML = '<a href="'+firefox_extension_url+'" class="btn btn-outline-dark bg-white btn-lg rounded-pill"><img src="images/browsers/firefox-icon.png" style="max-height: 32px;"/><b> Add to Firefox</b></a>';
+	}
 
 	// Google Chrome
-	// if( is_google_chrome() ){
-		// add_alby_btn_header.innerHTML = '<a href="'+google_play_url+'" class="btn btn-outline-dark bg-white rounded-pill px-4 my-4"><b>Add to Chrome</b></a>';
-		// add_alby_btn.innerHTML = '<a href="'+google_play_url+'" class="btn btn-outline-dark bg-white btn-lg rounded-pill"><img src="images/browsers/google-icon.png" style="max-height: 32px;"/><b> Add to Chrome</b></a>';
+	else if( is_google_chrome() ){
+		add_alby_btn_header.innerHTML = '<a href="'+google_play_url+'" class="btn btn-outline-dark bg-white rounded-pill px-4 my-4"><b>Add to Chrome</b></a>';
+		add_alby_btn.innerHTML = '<a href="'+google_play_url+'" class="btn btn-outline-dark bg-white btn-lg rounded-pill"><img src="images/browsers/google-icon.png" style="max-height: 32px;"/><b> Add to Chrome</b></a>';	
+	}
 
+	else{
 		add_alby_btn_header.innerHTML = '<a href="'+github_url+'" class="btn btn-outline-dark bg-white rounded-pill px-4 my-4"><b>Add to my browser</b></a>';
 		add_alby_btn.innerHTML = '<a href="'+github_url+'" class="btn btn-outline-dark bg-white btn-lg rounded-pill"><b>Add to my browser</b></a>';
-	// }
-	
+	}
 
 	//Display the icones
 	feather.replace();
@@ -34,17 +35,16 @@ $(document).ready(function () {
 
 
 
-// function is_firefox(){
-// 	if( typeof InstallTrigger !== 'undefined' ) {
-// 		return true;
-// 	}
-// 	return false;	
-// }
+function is_firefox(){
+	if( typeof InstallTrigger !== 'undefined' ) {
+		return true;
+	}
+	return false;	
+}
 
-
-// function is_google_chrome(){
-// 	if( !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime) ) {
-// 		return true;
-// 	}
-// 	return false;	
-// }
+function is_google_chrome(){
+	if( window.chrome !== null && typeof window.chrome !== "undefined" ) {
+		return true;
+	}
+	return false;
+}
