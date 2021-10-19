c07c3e5718c75f05da01893fe6496b3406086013
GBKS <chri@sto.ph>
2021-10-19 12:54:02 +0200

    Various styling and responsive tweaks

--- js/main.js
+++ js/main.js
@@ -1,49 +1,31 @@
-
-const google_play_url = "#";
-const firefox_extension_url = "#";
-
-const github_url = "https://github.com/getAlby/lightning-browser-extension#try-out-the-most-recent-version";
-
-
 $(document).ready(function () {
-
-	var add_alby_btn_header = document.getElementById("add_alby_btn_header");
-	var add_alby_btn = document.getElementById("add_alby_btn");
-
 	// Firefox
 	if( is_firefox() ) {
-		add_alby_btn_header.innerHTML = '<a href="'+firefox_extension_url+'" class="btn btn-outline-dark bg-white rounded-pill px-4 my-4"><b>Add to Firefox</b></a>';
-		add_alby_btn.innerHTML = '<a href="'+firefox_extension_url+'" class="btn btn-outline-dark bg-white btn-lg rounded-pill"><img src="images/browsers/firefox-icon.png" style="max-height: 32px;"/><b> Add to Firefox</b></a>';
+		document.body.classList.add('--firefox');
 	}
 
 	// Google Chrome
 	else if( is_google_chrome() ){
-		add_alby_btn_header.innerHTML = '<a href="'+google_play_url+'" class="btn btn-outline-dark bg-white rounded-pill px-4 my-4"><b>Add to Chrome</b></a>';
-		add_alby_btn.innerHTML = '<a href="'+google_play_url+'" class="btn btn-outline-dark bg-white btn-lg rounded-pill"><img src="images/browsers/google-icon.png" style="max-height: 32px;"/><b> Add to Chrome</b></a>';	
+		document.body.classList.add('--chrome');
 	}
 
 	else{
-		add_alby_btn_header.innerHTML = '<a href="'+github_url+'" class="btn btn-outline-dark bg-white rounded-pill px-4 my-4"><b>Add to my browser</b></a>';
-		add_alby_btn.innerHTML = '<a href="'+github_url+'" class="btn btn-outline-dark bg-white btn-lg rounded-pill"><b>Add to my browser</b></a>';
+		document.body.classList.add('--other');
 	}
 
-	//Display the icones
+	//Display the icons
 	feather.replace();
-
 });
 
-
-
-
 function is_firefox(){
-	if( typeof InstallTrigger !== 'undefined' ) {
+	if(typeof InstallTrigger !== 'undefined') {
 		return true;
 	}
 	return false;	
 }
 
 function is_google_chrome(){
-	if( window.chrome !== null && typeof window.chrome !== "undefined" ) {
+	if(window.chrome !== null && typeof window.chrome !== "undefined") {
 		return true;
 	}
 	return false;
